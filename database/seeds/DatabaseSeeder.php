95f092d81f5e3e22e97090d0f9f4fb7261ac4ecd
Luiz Manoel <luizresplandeoliveira@gmail.com>
2019-08-24 06:35:33 -0300

    :zap: atualização das migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,12 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            LinhaTeoricaSeeder::class,
-            PerfisSeeder::class,
-            UsersSeeder::class,
-            RoleSeeder::class,
-            PermissionsSeeder::class,
-            TesteSeeder::class
+            UserTableSeeder::class,
+            CityTableSeeder::class,
+            StateTableSeeder::class
         ]);
     }
 }
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use Illuminate\Database\Seeder;
+
+
+class UserTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('users')->insert(array (
+            0 =>
+                array (
+                    'name' => 'admin',
+                    'email' => 'admin@gmail.com',
+                    'password' => bcrypt('admin'),
+
+                ),
+            ));
+    }
+}
