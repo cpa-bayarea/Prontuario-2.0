2acacbbe50275868685590b29b84764fef5fa6d2
Luiz Manoel <luizresplandeoliveira@gmail.com>
2019-08-24 06:38:21 -0300

    :sparkles: publicando a pasta vendor do inspinia

---
+++ resources/views/vendor/inspinia/layouts/main-panel/footer/main.blade.php
@@ -0,0 +1,11 @@
+<div class="footer">
+  <div class="pull-right">
+    {{ config('app.name', 'Prontuário Eletrônico') }}
+  </div>
+  <div>
+   <strong>Bay Área </strong>- IESB &copy; {{ date('Y') }}
+  </div>
+
+
+
+</div>
---
+++ resources/views/vendor/inspinia/layouts/main.blade.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="@yield('lang', config('app.locale', 'pt-BR'))">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Atnic">
+
+    <title>@yield('title', config('app.name', 'Prontuário Eletrônico'))</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Styles -->
+    @section('styles')
+        <link href="{{ mix('/css/inspinia.css') }}" rel="stylesheet">
+        <link rel="stylesheet"
+              href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css"/>
+    @show
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+
+    <![endif]-->
+    @stack('head')
+</head>
+
+<body class="body-small {{ config('inspinia.skin', '') }}">
+<div id="wrapper">
+    @include('inspinia::layouts.sidebar.main')
+    @include('inspinia::layouts.main-panel.main')
+
+</div>
+
+@section('scripts')
+    <script src="{{ asset('js/jquery-3.4.1.min.js') }}" charset="utf-8"></script>
+    <script src="{{ mix('/js/manifest.js') }}" charset="utf-8"></script>
+    <script src="{{ mix('/js/vendor.js') }}" charset="utf-8"></script>
+    <script src="{{ mix('/js/inspinia.js') }}" charset="utf-8"></script>
+    <script src="{{ asset('js/jasny-bootstrap.min.js') }}" charset="utf-8"></script>
+    <script src="{{ asset('js/select2.min.js') }}" charset="utf-8"></script>
+    <script src="{{ asset('js/choose.jquery.js') }}" charset="utf-8"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
+@show
+@stack('body')
+
+@if(Session::has('error'))
+    <script>
+        $(document).ready(function () {
+            setTimeout(function () {
+                toastr.options = {
+                    closeButton: true,
+                    progressBar: true,
+                    showMethod: 'slideDown',
+                    timeOut: 4000
+                };
+                toastr.error('Ok!', '{{ Session::get('error') }}');
+            }, 1300);
+        });
+    </script>
+@endif
+
+@if(Session::has('info'))
+    <script>
+        $(document).ready(function () {
+            setTimeout(function () {
+                toastr.options = {
+                    closeButton: true,
+                    progressBar: true,
+                    showMethod: 'slideDown',
+                    timeOut: 4000
+                };
+                toastr.info('Ok!', '{{ Session::get('info') }}');
+            }, 1300);
+        });
+    </script>
+@endif
+
+@if(Session::has('warning'))
+    <script>
+        $(document).ready(function () {
+            setTimeout(function () {
+                toastr.options = {
+                    closeButton: true,
+                    progressBar: true,
+                    showMethod: 'slideDown',
+                    timeOut: 4000
+                };
+                toastr.warning('Ok!', '{{ Session::get('warning') }}');
+            }, 1300);
+        });
+    </script>
+@endif
+
+
+@if(Session::has('success'))
+    <script>
+        $(document).ready(function () {
+            setTimeout(function () {
+                toastr.options = {
+                    closeButton: true,
+                    progressBar: true,
+                    showMethod: 'slideDown',
+                    timeOut: 4000
+                };
+                toastr.success('Ok!', '{{ Session::get('success') }}');
+            }, 1300);
+        });
+    </script>
+@endif
+
+<script>
+
+    $('#uf').on('change', function () {
+        $.ajax({
+            url: "/search/cidadebyuf/"+$('#uf').val(),
+            type: "GET",
+        }).done(function (response) {
+
+           response.forEach(function(key){
+               $("#cidade").empty();
+               $("#cidade").append('<option value="'+key.id+'">'+key.title+'</option>');
+               $("#cidade").removeAttr( "disabled" );
+
+
+           });
+
+        })
+
+    });
+
+
+
+    $('.findById').on('click', function () {
+        $.ajax({
+            url: "search/paciente/findById/"+ $(this).val(),
+            type: "GET",
+        }).done(function (response) {
+
+            $('input[id=nome]').val(response.paciente.nome);
+            $('input[id=email]').val(response.paciente.email);
+            $('input[id=nome_social]').val(response.paciente.nome_social);
+            $('input[id=nome_responsavel]').val(response.paciente.nome_responsavel);
+            $('input[id=data_nascimento]').val(response.paciente.data_nascimento);
+            $('input[id=cpf]').val(response.paciente.cpf);
+            $('input[id=rg]').val(response.paciente.rg);
+            $('input[id=endereco]').val(response.paciente.endereco);
+            $("#uf").val(response.cidade[0].uf.id);
+            $("#uf").change();
+            $("#cidade option[value="+response.cidade[0].id+"]").attr("selected","selected");
+
+            $("#form_paciente").append('<input type="hidden" value="'+response.paciente.id+'" name="paciente_id">');
+            $('#button_submit').empty().append('Atualizar');
+
+
+
+
+        })
+
+    });
+
+
+
+</script>
+
+
+</body>
+</html>
