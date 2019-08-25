1beb51831643e11722943e6a1e6e0ac314af7f05
kelvin-Pereira <kelvinpereira.b18@gmail.com>
2019-08-25 00:14:48 -0300

    ADD :construction:  Menu triagem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,12 @@
   <ul class="nav metismenu" id="side-menu" style="padding-left:0px;">
     <li class="">
       <a href="{{ route('home') }}"><i class="fa fa-home"></i> <span class="nav-label">Home</span></a>
-
     </li>
     <li class="">
       <a href="{{ route('paciente') }}"><i class="fa fa-users"></i> <span class="nav-label">Pacientes</span></a>
+    </li>
+    <li class="">
+      <a href="{{ route('triagem') }}"><i class="fa fa-check-square-o"></i> <span class="nav-label">Triagem</span></a>
     </li>
   </ul>
 @endsection
