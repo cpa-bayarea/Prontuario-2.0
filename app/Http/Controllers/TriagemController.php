f9509caa7f1f1226600d7977b4f721fde70a4f22
kelvin-Pereira <kelvinpereira.b18@gmail.com>
2019-08-25 00:12:43 -0300

    ADD :construction:   Controler de triagem

---
+++ app/Http/Controllers/TriagemController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TriagemController extends Controller
+{
+    public function index()
+    {
+        return view('triagem.index');
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show()
+    {
+        //
+    }
+
+
+    public function edit()
+    {
+       
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+}
