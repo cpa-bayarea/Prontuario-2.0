b60e9f993d3ffe6f56d6943769467c36d451d2aa
Kelvin Pereira Biserra <42652204+Kelvin-Pereira@users.noreply.github.com>
2019-10-15 21:27:29 -0300

    UPDATE validando CPF

--- app/Http/Controllers/TriagemController.php
+++ app/Http/Controllers/TriagemController.php
@@ -14,6 +14,9 @@ use App\Telefone;
 use App\TriagemItensGrupo;
 use Illuminate\Support\Facades\DB;
 
+use App\Http\Requests\TriagemRequest;
+
+
 class TriagemController extends Controller
 {
     public function index() {
@@ -43,11 +46,31 @@ class TriagemController extends Controller
         return view('triagem.create',compact('supervisores',$supervisores,'alunos',$alunos,'grupos',$grupos,'tipoAtendimentos',$tipoAtendimentos,'temporario',$temporario,'paciente',$paciente));
     }
 
-    public function store(Request $request) {
+    public function store(TriagemRequest $request) {
 
         $paciente = new Paciente();
         $paciente->nome = $request->nome;
-        $paciente->cpf = $request->cpf;
+
+        if($this->validaCPF($request->cpf)) {
+            $paciente->cpf = $request->cpf;
+        }else {
+            Session::flash('success', 'Infome CPF Valido.');
+            $paciente = new Paciente();
+
+            $supervisores = Supervisor::orderBy('tx_nome', 'asc')->get();
+            $alunos = Aluno::orderBy('tx_nome', 'asc')->get();
+            $tipoAtendimentos = Grupo::find(1);
+            $grupos = Grupo::find(2);
+            $temporario = Grupo::find(3);
+
+            return view('triagem.create',compact('supervisores',$supervisores,
+                                                 'alunos',$alunos,
+                                                 'grupos',$grupos,
+                                                 'tipoAtendimentos',$tipoAtendimentos,
+                                                 'temporario',$temporario,
+                                                 'paciente',$paciente
+                                                ));
+        }
         $paciente->rg = $request->rg;
         $paciente->data_nascimento = $request->data_nascimento;
         $paciente->id_status = 1;
@@ -124,5 +147,31 @@ class TriagemController extends Controller
        
         Session::flash('success', 'Operação realizada com sucesso');
         return redirect(route('triagem'));
+    }
+
+    protected function validaCPF($cpf) {
+ 
+        // Extrai somente os números
+        $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
+         
+        // Verifica se foi informado todos os digitos corretamente
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+        // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+        // Faz o calculo para validar o CPF
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf{$c} * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf{$c} != $d) {
+                return false;
+            }
+        }
+        return true;
     }
 }
