d8731a1ce8ff1436eb0f11c20078808a75de5ce9
Luiz Manoel <luizresplandeoliveira@gmail.com>
2019-10-26 07:18:24 -0300

    :hammer: :construction: Criando Model e migration de Role e Permission

---
+++ app/Models/Role.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public function permissions(){
+        
+        return $this->belongsToMany('App\Permission','permission_roles');
+        
+    }
+}
---
+++ database/migrations/2019_10_26_100733_create_roles_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            
+            $table->increments('id');
+            $table->string('nome',50);
+            $table->string('descricao',200);
+            $table->timestamps();
+            
+        });
+        
+        Schema::create('role_users', function (Blueprint $table) {
+            
+            $table->increments('id');
+            
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+                
+            $table->integer('role_id')->unsigned();   
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles');
+            
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_users');
+        Schema::dropIfExists('roles');
+    }
+}
