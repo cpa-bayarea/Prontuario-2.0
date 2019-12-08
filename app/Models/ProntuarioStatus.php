84aee9968dc22afcf77ca76c7a187927a4acaf5f
Douglasx <douglasantana007@gmail.com>
2019-12-08 02:50:54 -0200

    [ADD]:rocket: Prontuario - Triagem #60.
    Constantes e modificações finais.

--- app/Models/ProntuarioStatus.php
+++ app/Models/ProntuarioStatus.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProntuarioStatus extends AbstractModel
 {
+    const P_STATUS_EM_ATENDIMENTO = 1;
+    const P_SOMENTE_TRIAGEM = 2;
+    const P_PLANTAO = 3;
+
     protected $table = 'prontuario_status';
     protected $fillable = ['nome'];
 }
